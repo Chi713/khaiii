7377f6fbd2548108811ed500496ea0e5aad36b7a
Chi713 <chris.hoskin2355@gmail.com>
2022-05-03 15:16:00 -0400

    modified to work with gcc 11+

--- src/test/cpp/test_main.cpp
+++ src/test/cpp/test_main.cpp
@@ -3,65 +3,63 @@
  * @copyright  Copyright (C) 2017-, Kakao Corp. All rights reserved.
  */
 
-
 //////////////
 // includes //
 //////////////
 #include <iostream>
+#include <limits>
 
 #include "cxxopts.hpp"
 #include "fmt/printf.h"
 #ifdef PROFILER
-    #include "gperftools/profiler.h"
+#include "gperftools/profiler.h"
 #endif
-#include "gtest/gtest.h"
 #include "spdlog/spdlog.h"
+#include "gtest/gtest.h"
 
 #include "khaiii/khaiii_dev.h"
 
-
 using std::cerr;
 using std::string;
-
 
 ///////////////
 // variables //
 ///////////////
 // global variable for program arguments
-cxxopts::ParseResult* prog_args;
-
+cxxopts::ParseResult *prog_args;
 
 //////////
 // main //
 //////////
-int main(int argc, char** argv) {
-    cxxopts::Options options(argv[0], argv[0]);
-    testing::InitGoogleTest(&argc, argv);
-    auto _log = spdlog::stderr_color_mt("console");
-    spdlog::set_level(spdlog::level::warn);
+int main(int argc, char **argv) {
+  cxxopts::Options options(argv[0], argv[0]);
+  testing::InitGoogleTest(&argc, argv);
+  auto _log = spdlog::stderr_color_mt("console");
+  spdlog::set_level(spdlog::level::warn);
 
-    options.add_options()
-        ("h,help", "print this help")
-        ("rsc-dir", "resource directory", cxxopts::value<string>()->default_value("./share/khaiii"))
-        ("set-log", "set log level", cxxopts::value<string>()->default_value("all:warn"));
-    auto args = options.parse(argc, argv);
+  options.add_options()("h,help", "print this help")(
+      "rsc-dir", "resource directory",
+      cxxopts::value<string>()->default_value("./share/khaiii"))(
+      "set-log", "set log level",
+      cxxopts::value<string>()->default_value("all:warn"));
+  auto args = options.parse(argc, argv);
 
-    if (args.count("help")) {
-        fmt::fprintf(cerr, "%s\n", options.help());
-        return 0;
-    }
-    prog_args = &args;
-    khaiii_set_log_levels(args["set-log"].as<string>().c_str());
-
-#ifdef PROFILER
-    ProfilerStart("/tmp/test_khaiii.prof");
-#endif
-
-    int ret = RUN_ALL_TESTS();
+  if (args.count("help")) {
+    fmt::fprintf(cerr, "%s\n", options.help());
+    return 0;
+  }
+  prog_args = &args;
+  khaiii_set_log_levels(args["set-log"].as<string>().c_str());
 
 #ifdef PROFILER
-    ProfilerStop();
+  ProfilerStart("/tmp/test_khaiii.prof");
 #endif
 
-    return ret;
+  int ret = RUN_ALL_TESTS();
+
+#ifdef PROFILER
+  ProfilerStop();
+#endif
+
+  return ret;
 }
